0ae232438061f3c3d66c41ccedf239646e6f454a
Miharu Naruse <nars024@gmail.com>
2022-07-14 00:02:43 +0900

    :sparkles: feat: Answer 001

--- LayoutGame/Try001View.swift
+++ LayoutGame/Try001View.swift
@@ -5,12 +5,31 @@
 //  Created by MiharuNaruse on 2022/07/13.
 //
 
+// `layoutPriority(_:)`
+// https://developer.apple.com/documentation/swiftui/path/layoutpriority(_:)
+
 import SwiftUI
 
 struct Try001View: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Hello, world!")
+                    .layoutPriority(1)
+                Spacer()
+                Text("Hello, world!")
+            }
+            Spacer()
+            Text("Hello, world!")
+            Spacer()
+            HStack {
+                Text("Hello, world!")
+                Spacer()
+                Text("Hello, world!")
+                    .layoutPriority(1)
+            }
+        }
+        .padding()
     }
 }
 
