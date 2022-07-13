f840f57bfaff160ca7e05bbc1d788bb81d804c9e
Miharu Naruse <nars024@gmail.com>
2022-07-14 00:04:42 +0900

    :wrench: chore: Add Apple WatchKit Extension

---
+++ LayoutGameWatchApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  LayoutGameWatchApp WatchKit Extension
+//
+//  Created by MiharuNaruse on 2022/07/13.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
